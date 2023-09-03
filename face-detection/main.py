8270d5f8b6e15c1dccb57aa559ff0c3964475c15
yektaokdan <57546227+yektaokdan@users.noreply.github.com>
2023-09-03 03:44:27 +0300

    added

---
+++ face-detection/main.py
@@ -0,0 +1,22 @@
+from cvzone.FaceDetectionModule import FaceDetector
+import cv2
+
+cap = cv2.VideoCapture(0)
+detector = FaceDetector()
+
+while True:
+    success, img = cap.read()
+    img, bboxs = detector.findFaces(img)
+
+    if bboxs:
+        center = bboxs[0]["center"]
+        cv2.circle(img, center, 5, (255, 0, 255), cv2.FILLED)
+
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+# run 'pip3 install mediapipe' code in terminal before running
+# calistirmadan once terminalde 'pip3 install mediapipe' kodunu calistirin.
